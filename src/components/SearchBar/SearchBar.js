dbe6fa402af218bc73eb5644f2fe4a328d4c673a
Artiom Kelbuts <artiomkelbuts@gmail.com>
2024-03-14 11:16:30 +0200

    Add SearchBar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import './SearchBar.css';
+
+
+function SearchBar() {
+  return (
+    <div className="SearchBar">
+      <input type="text" placeholder="Search title..." />
+    </div>
+  );
+}
+
+export default SearchBar;
