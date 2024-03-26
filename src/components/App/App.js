c0053395f9bf33b79673c964394e9012480fc3d0
Artiom Kelbuts <artiomkelbuts@gmail.com>
2024-03-26 14:27:05 +0200

    Refresh token

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
+
+
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -10,26 +12,6 @@ import SearchResults from "../SearchResults/SearchResults";
 import PlayList from "../Playlist/Playlist";
 import Spotify from "../../util/Spotify";
 
-const TRACKS = [
-  {
-    name: "Relic",
-    artist: "Vice State",
-    album: "Relic",
-    id: "3toHaLUlHzXqoduPCDVQZp",
-  },
-  {
-    name: "Fallen Angel",
-    artist: "KETTAMA",
-    album: "Fallen Angel",
-    id: "67zHx368N96FXQrz0iXzWO",
-  },
-  {
-    name: "Sugar",
-    artist: "Kincaid",
-    album: "Sugar",
-    id: "3WVYDLSVgs1mnUsGeI0ezM",
-  },
-];
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
@@ -39,10 +21,6 @@ function App() {
 
   const search = () => {
     Spotify.search(searchTerm).then(setSearchResults);
-  };
-
-  const setTracks = () => {
-    setPlayListTracks(TRACKS);
   };
 
   const addTrack = (track) => {
@@ -64,11 +42,14 @@ function App() {
     });
   };
 
+  useEffect(() => {
+    Spotify.init()
+  });
+
   return (
     <div className="App">
       <SearchBar
         onSearchButton={search}
-        setTracks={setTracks}
         onSearchTermUpdate={setSearchTerm}
       />
       <SearchResults searchResults={searchResults} onAdd={addTrack} />
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,15 +1,54 @@
+/*global location*/
+/*eslint no-restricted-globals: ["error"]*/
+
 const clientID = "cc33a35a08ea4f2f9f92ba131ba5fbc2";
 const clientSecret = "53d868c11972452580edb41f1b89ac53";
 const accessToken =
   "BQC21qdAmS1nh6pA2AJ_O_MPfqwS6Su-1ah-nsk7UOPE_hONCmvITlUoRS1rni0YOSXEsY8oKJo5dZzqdvMCJIKsr8Np8SlLd_XyljWh6Qn99NPh0KAm2XST2u2780Uc60t7KvvvT2HlCNrswfQD1s0TkfSX5eND6yafllclsw7qWBrbSw9vcEUwNZwi55JQ0uNkC8FEoqBmVDJYAOyg3SIRsPSk3kyxRhFVamrnIFuiXxu7N6HF7QXPFEdKMmil";
 const apiRoot = "https://api.spotify.com/v1/";
+let token;
 
-// 'https://accounts.spotify.com/authorize?scope=playlist-modify-private%20playlist-modify-public&response_type=token&client_id=cc33a35a08ea4f2f9f92ba131ba5fbc2&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F'
+const spotifyUrl = `https://accounts.spotify.com/authorize?scope=playlist-modify-private%20playlist-modify-public&response_type=token&client_id=${clientID}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F`;
 
 const Spotify = {
+  init() {
+    //if token in url, save it and replace url
+    const params = "?" + location.href.split("#")[1];
+    const urlParams = new URLSearchParams(params);
+    token = urlParams.get("access_token");
+    console.log(token);
+    if (token != null) {
+      //save token locally
+      localStorage.setItem("token", token);
+      //replace url
+      location = location.href.split("#")[0];
+    }
+
+    //get saved token
+    token = localStorage.getItem("token");
+    //check token valid
+    fetch(`${apiRoot}me`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        return data.id;
+      })
+      .then((userId) => {
+        if (userId == null) {
+          //if not valid, open spotify grant page
+          location = spotifyUrl;
+        }
+      });
+    
+  },
+
   search(term) {
     return fetch(`${apiRoot}search?q=${term}&type=track`, {
-      headers: { Authorization: `Bearer ${accessToken}` },
+      headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => {
         return response.json();
@@ -28,7 +67,7 @@ const Spotify = {
   savePlaylist(playListName, playListTracks) {
     // get user id
     return fetch(`${apiRoot}me`, {
-      headers: { Authorization: `Bearer ${accessToken}` },
+      headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => {
         return response.json();
@@ -41,7 +80,7 @@ const Spotify = {
         // create new playlist
         fetch(`${apiRoot}users/${userId}/playlists`, {
           method: "POST",
-          headers: { Authorization: `Bearer ${accessToken}` },
+          headers: { Authorization: `Bearer ${token}` },
           body: JSON.stringify({ name: playListName, public: false }),
         })
           .then((response) => {
@@ -55,7 +94,7 @@ const Spotify = {
             // set tracks
             fetch(`${apiRoot}playlists/${playListId}/tracks`, {
               method: "POST",
-              headers: { Authorization: `Bearer ${accessToken}` },
+              headers: { Authorization: `Bearer ${token}` },
               body: JSON.stringify({
                 uris: playListTracks.map((item) => item.uri),
               }),
