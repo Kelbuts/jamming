41253c9d7367702f0bf35fb781e91928d988b84a
Artiom Kelbuts <artiomkelbuts@gmail.com>
2024-03-26 15:28:17 +0200

    Add button sound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import PlayList from "../Playlist/Playlist";
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,12 +1,21 @@
 import "./Track.css";
+import useSound from 'use-sound';
+import add from '../../sounds/bite.mp3';
+import del from '../../sounds/disable-sound.mp3';
+
+
 
 function Track(props) {
+  const [addPlay] = useSound(add);
+  const [delPlay] = useSound(del);
   const addTrack = () => {
     props.onAdd(props.track);
+    addPlay();
   };
-
+   
   const removeTrack = () => {
     props.onRemove(props.track);
+    delPlay();
   };
 
   const renderAction = () => {
